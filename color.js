edfdcbda074e615bd63a7116104b894e85f122b4
ROMEEZHOU <yz7258@nyu.edu>
2023-02-10 07:27:05 +0800

    popup added, currently having trouble executing color.js

--- color.js
+++ color.js
@@ -9,12 +9,23 @@
     }
     window.hasRun = true;
 
-
+    const day = {
+        colors: {
+          frame: '#CF723F',
+          tab_background_text: '#111',
+        }
+    };
+    
+    
+    function updateThemeForCurrentWindow() {
+      let currentWindow = await browser.windows.getLastFocused();
+      browser.theme.update(currentWindow.id, day);
+    }
     /**
      * Given a color, remove existing tab color and change to new color.
      */
-    function setColor() {
-
+    function setColor(chosen_color) {
+        browser.theme.update(currentWindow.id, day);
     }
 
     /**
@@ -23,19 +34,19 @@
     function removeColor() {
 
     }
-
+    
     /**
      * Listen for messages from the background script.
      * Call "setColor()" or "removeColor()".
      */
     browser.runtime.onMessage.addListener((message) => {
         if (message.command === "color") {
-            setColor();
+            setColor(message.chosen_color);
         } else if (message.command === "reset") {
             removeColor();
         }
     });
 
-})();
+});
 
 
