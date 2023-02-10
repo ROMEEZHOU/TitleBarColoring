271807bcc01420a5063da02b4f550f9c35278ff8
ROMEEZHOU <yz7258@nyu.edu>
2023-02-11 02:26:17 +0800

    able to set toolbar color to red/blue or reset according to user choice

--- color.js
+++ color.js
@@ -1,10 +1,10 @@
-function themeWindow(window) {
+function themeWindow_red(window) {
     browser.theme.update(window.id, {
         images: {
           theme_frame: "",
         },
         colors: {
-          frame: "black",
+          frame: "white",
           tab_background_text: "white",
           toolbar: "#b72f0e",
           toolbar_text: "white"
@@ -12,10 +12,43 @@ function themeWindow(window) {
       });
     }
 
+function themeWindow_blue(window) {
+    browser.theme.update(window.id, {
+        images: {
+            theme_frame: "",
+        },
+        colors: {
+            frame: "white",
+            tab_background_text: "white",
+            toolbar: "#3652f4",
+            toolbar_text: "white"
+        }
+        });
+    }
+
+function themeWindow_default(window) {
+        browser.theme.reset(window.id)
+    }
+
 function handleMessage(request, sender, sendResponse) {
     console.log(`A content script sent a message: ${request.greeting}`);
     sendResponse({ response: "Response from background script" });
-    browser.windows.getAll().then(wins => wins.forEach(themeWindow));
+    switch (request.greeting) {
+        case "Red":
+            console.log(request.greeting);
+            console.log(request.greeting === "Red")
+            console.log("change the theme to red");
+            browser.windows.getAll().then(wins => wins.forEach(themeWindow_red));
+            break;
+        case "Blue":
+            console.log(request.greeting);
+            console.log(request.greeting === "Blue")
+            console.log("change the theme to blue")
+            browser.windows.getAll().then(wins => wins.forEach(themeWindow_blue));
+            break;
+        default:
+            browser.windows.getAll().then(wins => wins.forEach(themeWindow_default));
+        }
     console.log("changed theme");
   }
   
